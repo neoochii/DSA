bef89506e8e39eda2787fe464c2df04ee5e03afd
Dipak Das <98222877+neoochii@users.noreply.github.com>
2025-07-13 18:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (59.04%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int count = 0;
+
+        int[] arr = new int[n*2];
+        for(int i =0;i< n;i++){
+           arr[count]= nums[i];
+           arr[count+1]= nums[i+n];
+           count += 2;
+        }
+        return arr;
+        
+    }
+}
