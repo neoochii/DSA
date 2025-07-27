d688617c5f5ba309c674783478edbf62f364be7f
Dipak Das <98222877+neoochii@users.noreply.github.com>
2025-07-27 21:59:11 +0530

    Time: 1 ms (98.16%) | Memory: 41 MB (68.18%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,11 +1,26 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int row = 0;
+        int start = 0;
+        int end = n;
+        int max = 0;
 
-        while( n >= row + 1){
-            row++;
-            n = n - row;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long coins = formula(mid); // use long to avoid overflow
+
+            if (coins == n) {
+                return mid;
+            } else if (coins > n) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                max = mid;
+            }
         }
-        return row;
+        return max;
+    }
+
+    private long formula(int mid) {
+        return (long) mid * (mid + 1) / 2;
     }
 }
