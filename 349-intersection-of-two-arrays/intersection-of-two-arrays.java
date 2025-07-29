b695614c55a35401e22508d0d81429ffe3980ff4
Dipak Das <98222877+neoochii@users.noreply.github.com>
2025-07-29 11:29:17 +0530

    Time: 4 ms (28.66%) | Memory: 43.6 MB (35.49%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,23 +1,41 @@
+import java.util.*;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums2); // Step 1: Sort nums2
 
-     Set<Integer> set1 = new HashSet<>();
-     for(int num : nums1){
-        set1.add(num);
-     }
+        Set<Integer> resultSet = new HashSet<>();
+        Set<Integer> visited = new HashSet<>(); // to avoid duplicates in nums1
 
-     Set<Integer> resultSet = new HashSet<>();
-     for(int num: nums2){
-        if(set1.contains(num)){
-            resultSet.add(num);
+        for (int num : nums1) {
+            if (!visited.contains(num)) {
+                visited.add(num);
+                if (binarySearch(nums2, num)) {
+                    resultSet.add(num);
+                }
+            }
         }
-     }
-     int[] result = new int[resultSet.size()];
-     int i =0;
-     for(int num : resultSet){
-        result[i++] = num;
-     }
-     return result;
-        
+
+        // Convert set to array
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    private boolean binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) return true;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
     }
 }
