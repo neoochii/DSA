42f79670922ae39863d99f8133429f9577484ae6
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-05-16 07:18:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (34.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,24 @@
 class Solution {
     static  int []term = new int [1000]; 
     public int climbStairs(int n) {
-        if(n < 0){
+       
+        int[] dp = new int[ n+1];
+        return solve(n, dp);
+    }
+
+    int solve(int n , int dp[]){
+         if(n < 0){
             return 0;
         }
         if(n == 0){
             return 1;
         }
-            if (term[n] != 0)
-        return term[n];
-        int onestep = climbStairs(n -1);
-        int twostep = climbStairs(n - 2);
 
-        return   term[n] = onestep + twostep;
+        if(dp[n] != 0){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-2 , dp) + solve(n -1 , dp);
+
     }
 }
