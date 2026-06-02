14d33f976b907cb70d4941b2098e18ac9e350826
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-06-02 21:19:16 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (61.51%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,20 +1,26 @@
 class Solution {
     public int earliestFinishTime(int[] startL, int[] durL, int[] startW, int[] durW) {
-        int minL =  Integer.MAX_VALUE, minW = minL, res = minW;
-        int n = startL.length;
-        int m = startW.length;
+   int miL = Integer.MAX_VALUE,miW = miL , res = miW ;
 
-        for (int i = 0; i < n; i++)
-            minL = Math.min(minL, startL[i] + durL[i]);
+   int L = startL.length;
+   int W = startW.length;
 
-        for (int i = 0; i < m; i++) {
-            minW = Math.min(minW, startW[i] + durW[i]);
-            res = Math.min(res, Math.max(minL, startW[i]) + durW[i]);
-        }
+   for(int i = 0 ; i< L ;i++){
+    miL = Math.min(miL , startL[i]  + durL[i]);
 
-        for (int i = 0; i < n; i++)
-            res = Math.min(res, Math.max(minW, startL[i]) + durL[i]);
 
-        return res;
+   }
+   for(int i = 0 ; i < W ; i++){
+    miW = Math.min(miW ,startW[i] + durW[i]);
+    res = Math.min(res , Math.max(miL , startW[i]) + durW[i]);
+   }
+
+   for(int i = 0; i < L ; i++){
+    res = Math.min(res, Math.max(miW , startL[i])+durL[i]);
+
+   }
+   return res;
+
+
     }
 }
