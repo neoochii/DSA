88715f40d8d636801ec677ee06ae162c193dee5d
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-06-05 01:09:32 +0530

    Time: 28 ms (48.18%) | Memory: 46.5 MB (37.59%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,38 +1,29 @@
 class Solution {
     public int totalWaviness(int num1, int num2) {
-         
-         
-
 
         int sol = 0;
-         
 
-        for(int j = num1 ; j <= num2 ; j++){
-             String s = String.valueOf(Math.abs(j));
-              int length = s.length();
-               int mid = length / 2;
-            
+        for (int j = num1; j <= num2; j++) {
 
-            if(length < 3){
+            String s = String.valueOf(Math.abs(j));
+
+            if (s.length() < 3)
                 continue;
-            }
-            
 
-                for(int i = 1 ; i<= length - 2; i++){
-                    if(Character.getNumericValue(s.charAt(i)) >Character.getNumericValue(s.charAt(i-1)) &&Character.getNumericValue(s.charAt(i)) >Character.getNumericValue(s.charAt(i+1))){
-                        sol++;
-                    }
-                     if(Character.getNumericValue(s.charAt(i)) <Character.getNumericValue(s.charAt(i-1)) &&Character.getNumericValue(s.charAt(i))<Character.getNumericValue(s.charAt(i+1))){
-                        sol++;
-                    }
+            for (int i = 1; i < s.length() - 1; i++) {
+
+                int prev = s.charAt(i - 1) - '0';
+                int curr = s.charAt(i) - '0';
+                int next = s.charAt(i + 1) - '0';
+
+                if ((curr > prev && curr > next) ||
+                    (curr < prev && curr < next)) {
+
+                    sol++;
                 }
-
-                
-            
-
-
-
+            }
         }
+
         return sol;
     }
 }
