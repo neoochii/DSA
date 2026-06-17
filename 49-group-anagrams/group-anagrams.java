2e3b3d9b283baff15770d7cad3d02ffb0671a4f5
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-06-17 18:15:14 +0530

    Time: 21 ms (10.65%) | Memory: 50.1 MB (23.46%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,9 +3,18 @@ class Solution {
         Map<String, List<String>> ans = new HashMap<>();
 
         for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+            int[] count = new int[26];
+
+            // Count frequency of each letter in the string
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int num : count) {
+                sb.append(num).append("#");
+            }
+            String key = sb.toString();
             if (!ans.containsKey(key)) {
                 ans.put(key, new ArrayList<>());
             }
