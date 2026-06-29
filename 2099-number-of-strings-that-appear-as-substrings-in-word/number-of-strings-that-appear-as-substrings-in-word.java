f9be936bef4ff5612dfdf9a64cc3d59481444483
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-06-29 11:01:10 +0530

    Time: 1 ms (75.75%) | Memory: 43.4 MB (56.03%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,27 +1,14 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-         Map<Integer, String> map = new HashMap<>();
-        int counter = 1;
-
-        // Nested loops to find all substrings
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = i + 1; j <= word.length(); j++) {
-                // Extract substring
-                String sub = word.substring(i, j);
-                
-                // Add to map using a unique counter as the key
-                map.put(counter, sub);
-                counter++;
-            }
-        }
         int matchCount = 0;
 
-
-        for(String ch : patterns ){
-             if (map.containsValue(ch)) {
+        for (String pat : patterns) {
+            // Check directly if the pattern exists inside the word
+            if (word.contains(pat)) {
                 matchCount++;
             }
         }
+        
         return matchCount;
     }
 }
