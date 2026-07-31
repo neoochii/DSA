1d8b0573b759981f894b79c99f40533075265109
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-07-31 11:41:20 +0530

    Time: 7 ms (1.45%) | Memory: 43.9 MB (5.56%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minimumPushes(String word) {
-        if(word.length() <= 8){
-            return word.length();
+     int[] mp = new int[26];
+
+        // Generic solution
+        for (char ch : word.toCharArray()) {
+            mp[ch - 'a']++; // Mentioned in the question, all letters will be distinct
         }
-        int count = 0 ; 
-        Map<Integer , Integer> mp = new HashMap<>();
-        int assign = 2; 
-        for(char ch : word.toCharArray()){
-            if(assign > 9){
-                assign = 2;
-            }
-            mp.put(assign ,mp.getOrDefault(assign , 0)+ 1);
-            count += mp.get(assign);
-            assign++;
+
+        // Descending order of frequency
+        Integer[] mpInteger = Arrays.stream(mp).boxed().toArray(Integer[]::new);
+        Arrays.sort(mpInteger, Comparator.reverseOrder());
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans += mpInteger[i] * ((i / 8) + 1);
         }
-        return count;
+        return ans;
     }
 }
