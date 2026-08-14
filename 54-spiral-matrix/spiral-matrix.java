a0be5a2a27fabf7cc28fd85d717c4babd7472676
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-08-14 14:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (9.72%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,53 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-       // Handle empty or null matrix
-        if (matrix == null || matrix.length == 0) {
-            return new ArrayList<>();
-        }
-
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int left = 0;
-        int right = m - 1;
+       int m = matrix.length;
+       int n = matrix[0].length;
         int top = 0;
-        int bottom = n - 1;
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        while (top <= bottom && left <= right) {
-            // Traverse Right
-            for (int i = left; i <= right; i++) {
+        int down = m -1;
+        int left = 0;
+        int right = n - 1;
+        List<Integer> ans = new ArrayList<>();
+        while(left <= right && top <= down){
+            for(int i = left; i <= right ; i++){
                 ans.add(matrix[top][i]);
             }
             top++;
-
-            // Traverse Down
-            for (int i = top; i <= bottom; i++) {
+            for(int i = top ; i <= down ;i++){
                 ans.add(matrix[i][right]);
             }
             right--;
-
-            // Traverse Left (Check boundary before proceeding)
-            if (top <= bottom) {
-                // CORRECTED LOOP: Decrement from right to left
-                for (int i = right; i >= left; i--) {
-                    ans.add(matrix[bottom][i]);
-                }
-                bottom--;
+           if(top <= down){
+             for(int i = right ; i >= left; i--){
+                ans.add(matrix[down][i]);
             }
+            down--;
+           }
+           if(left <= right){
 
-            // Traverse Up (Check boundary before proceeding)
-            if (left <= right) {
-                // CORRECTED LOOP: Decrement from bottom to top
-                for (int i = bottom; i >= top; i--) {
-                    ans.add(matrix[i][left]);
-                }
-                left++;
+            for(int i =down ; i >= top ; i-- ){
+                ans.add(matrix[i][left]);
             }
+            left++;
+           }
+
+
         }
         return ans;
-     
 
-
-        
     }
 }
