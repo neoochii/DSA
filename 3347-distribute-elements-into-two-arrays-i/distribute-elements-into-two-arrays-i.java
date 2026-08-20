13e4e9e4aed7a1a537d3c910b4f6624a81e1331e
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-08-20 09:35:03 +0530

    Time: 2 ms (57.45%) | Memory: 46.9 MB (12.74%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -2,23 +2,24 @@ class Solution {
     public int[] resultArray(int[] nums) {
         ArrayList<Integer> arr1 = new ArrayList<>();
         ArrayList<Integer> arr2 = new ArrayList<>();
+
         arr1.add(nums[0]);
         arr2.add(nums[1]);
 
-
-        for(int  i =2; i< nums.length ;i++){
-
-
-           if( arr1.getLast() > arr2.getLast()){
+        for (int i = 2; i < nums.length; i++) {
+            if (arr1.getLast() > arr2.getLast()) {
                 arr1.add(nums[i]);
             } else {
                 arr2.add(nums[i]);
             }
         }
-        int[] result = Stream.concat(arr1.stream(), arr2.stream())
-                     .mapToInt(Integer::intValue)
-                     .toArray();
-       return result;
 
+        int[] ans = new int[nums.length];
+        int k = 0;
+
+        for (int x : arr1) ans[k++] = x;
+        for (int x : arr2) ans[k++] = x;
+
+        return ans;
     }
 }
