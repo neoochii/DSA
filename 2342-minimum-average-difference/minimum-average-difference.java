9cb1ca9066ddbc9b4fc47227178a7b2770d66899
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-08-24 23:12:46 +0530

    Time: 5 ms (96.93%) | Memory: 92.6 MB (77.71%) - LeetSync

---
+++ 2342-minimum-average-difference/minimum-average-difference.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int n = nums.length;
+        long total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        long prefix = 0;
+        long minDiff = Long.MAX_VALUE;
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+
+            long leftAvg = prefix / (i + 1);
+
+            long rightAvg = 0;
+
+            if (i != n - 1) {
+                rightAvg = (total - prefix) / (n - i - 1);
+            }
+
+            long diff = Math.abs(leftAvg - rightAvg);
+
+            if (diff < minDiff) {
+                minDiff = diff;
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
