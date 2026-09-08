7ce67130889126f18569f9536c5d93c6a0ab9b61
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-09-08 14:18:04 +0530

    Time: 1 ms (99.34%) | Memory: 42.6 MB (70.86%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,13 @@
+   
+   class Solution {
+    public long countCommas(long n) {
+         long res = 0;
+        long   p = 1000;
+        for (int i = 1; i <= 5; ++i) {
+            if (n >= p) {
+                res += n - p + 1;
+            }
+            p *= 1000;
+        }
+        return res;
+    }}
