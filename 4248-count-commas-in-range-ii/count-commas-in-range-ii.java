c6f1c3b12cbce444634f6fdbf1c9de28af2ee131
Dipak Das <98222877+neoochii@users.noreply.github.com>
2026-09-09 21:16:47 +0530

    Time: 1 ms (99.36%) | Memory: 42.5 MB (71.15%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,7 +1,14 @@
    
    class Solution {
+
+    /*so here we get comma after every 10power3 , or 1000; 
+    till 10^3  to 10^6 -1 ,
+         10^6  to 10^9 -1 ,, two(commas ) 
+
+
+    */
     public long countCommas(long n) {
-         long res = 0;
+      long res = 0;
         long   p = 1000;
         for (int i = 1; i <= 5; ++i) {
             if (n >= p) {
